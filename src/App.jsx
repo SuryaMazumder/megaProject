c158faaead13975cad44112d9ab0061960117c38
suryamazumder <suryakantamazumder9@gmail.com>
2024-04-20 10:44:26 +0530

    mega project 1st commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import conf from './conf/conf'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    {console.log(conf.appwriteUrl)}
+      <div>Appwrite bolta hai</div>
+    </>
+  )
+}
+
+export default App
