ba863db36840c29dbff8c835e119dc40ac7d3bf7
suryamazumder <suryakantamazumder9@gmail.com>
2024-04-29 23:58:56 +0530

    code for auth,app and componets

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -1,1 +1,67 @@
+/* eslint-disable no-useless-catch */
+import { Client, Account, ID } from "appwrite";
+import conf from "../conf/conf";
 
+export class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+
+    this.account = new Account(this.client);
+  }
+
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+      if (userAccount) {
+        //call another method
+        return this.login({ email, password });
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      return await this.account.createSession(email, password);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      console.log("Get current User:", error);
+      throw error;
+    }
+    // eslint-disable-next-line no-unreachable
+    return null;
+  }
+
+  async logout() {
+    try {
+      await this.account.deleteSessions();
+    } catch (error) {
+      console.log("appwrite:logout", error);
+      throw error;
+    }
+  }
+}
+
+const authService = new AuthService();
+
+export default authService;
